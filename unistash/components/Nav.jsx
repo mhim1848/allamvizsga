271e904474505049840439e415cdae8455791fd3
unknown <hunormth@gmail.com>
2022-04-05 17:43:43 +0300

    Elkezdtem dolgozni gecc

--- unistash/components/Nav.jsx
+++ unistash/components/Nav.jsx
@@ -1,8 +1,25 @@
+import Link from "next/link";
+
 export default function Nav() {
-	return (
-		<>
-			{/* CSS class property is className in jsx */}
-			<h1 className="text-white bg-black block">Navbar</h1>
-		</>
-	);
+  return (
+    <>
+      <div className="text-white bg-black block p-2 space-x-3 group">
+        {/* CSS class property is className in jsx */}
+        <span>Navbar</span>
+
+        <Link href="/login">
+          <a className="hover:bg-gray-600">Login</a>
+        </Link>
+        <Link href="/reg">
+          <a className="hover:bg-gray-600">Registration</a>
+        </Link>
+        <Link href="/courses">
+          <a className="hover:bg-gray-600">Courses</a>
+        </Link>
+        <Link href="/faq">
+          <a className="hover:bg-gray-600">FAQ</a>
+        </Link>
+      </div>
+    </>
+  );
 }
---
+++ unistash/pages/login.jsx
@@ -0,0 +1,5 @@
+import { Form, Formik, ErrorMessage, Field } from "formik";
+
+export default function Login(props) {
+  return <></>;
+}
